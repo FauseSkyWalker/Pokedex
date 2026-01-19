ecd7358ba5177921ac2c59636c54f447daa78aaa
FauseSkyWalker <Fausejr@gmail.com>
2026-01-19 11:05:56 -0300

    Feat(Doc): documentação do projeto

--- pokedex/app/main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from app.views import pokemon_router
-
-app = FastAPI()  # Cria a aplicação FastAPI
-app.include_router(pokemon_router, prefix="/pokemon")  # Registra as rotas da Pokédex
